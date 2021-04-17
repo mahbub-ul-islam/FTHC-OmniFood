ad7aadf889e34fbe74c2aa6b1100e2d52750965b
Mahbub Ul Islam <mahbub_ul_islam@outlook.com>
2021-04-18 01:13:30 +0600

    Create script.js

---
+++ resources/js/script.js
@@ -0,0 +1,27 @@
+
+$(document).ready(function() {
+
+    $('h1').click(function() {
+        $(this).css('background-color', '#fff000');
+    });
+
+});
+
+
+$(document).ready(function() {
+    $('h2').click(function() {
+        $(this).css('background-color', '#fff')
+    })
+})
+
+
+
+
+
+
+
+
+
+
+
+
